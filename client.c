64dbe7257b6c2c8542ff384503a997082ae40df7
MikeDog <576268911@qq.com>
2015-12-13 11:42:10 +0800

    ccs

--- client.c
+++ client.c
@@ -37,12 +37,12 @@ void str_cli(FILE *fp, int sockfd){
 		FD_SET(fileno(fp),&reset);
 		FD_SET(sockfd, &rset);
 		maxfdp1=max(fileno(fp),sockfd)+1;
-		select(max,&rset,NULL,NULL,NULL);
+		Select(max,&rset,NULL,NULL,NULL);
 		if(FD_ISSET(sockfd,&rset)){
-			if(readline(sockfd,recvline,MAXLINE) == 0){
+			if(Readline(sockfd,recvline,MAXLINE) == 0){
 				err_quit("str_cli: server terminated prematurely");		
 			}
-			fputs(recvline,stdout);// print connect infomation
+			Fputs(recvline,stdout);// print connect infomation
 		}
 		if(FD_ISSET(fileno(fp),&rset)){
 			if(FD_ISSET(fileno(fp),&rset)){
